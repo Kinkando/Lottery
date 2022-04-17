15b7b076c8d3489922fdf2e4fbe6dc24fa0d1ff4
Tanawat Yuwansiri <TY_Sage@hotmail.com>
2022-04-18 00:17:12 +0700

    clean code

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:lottery/models/lottery.dart';
 import 'package:lottery/views/widgets/my_scaffold.dart';
-import 'package:lottery/services/api.dart';
-
-// Future<List<dynamic>> _fetchLottery() async {
-//   String date = "2565-01-17";
-//   List list = await Api().fetch('server.php', queryParams: {"drawdate": date});
-//   final result = list.map((item) => Lottery.fromJson(item, date)).toList();
-//   print(date);
-//   result.forEach((element) {
-//     print(element.id);
-//     print(element.name);
-//     print(element.reward);
-//     print(element.amount);
-//     print(element.number);
-//   });
-//   return result;
-// }
-
-void _fetch() async {
-  final Map<String, dynamic> result = await Api().fetch('lotterys');
-  final lottery = Lottery.fromJson(result);
-  print(lottery.drawDate);
-  lottery.lottery.forEach((element) {
-    print(element.name);
-    print(element.reward);
-    print(element.number);
-  });
-}
 
 void main() {
-  // _fetchLottery();
-  // _fetch();
   runApp(const MyApp());
 }
 
@@ -44,19 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Lottery Unikuma',
+      title: 'Lottery Unikume',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const MyScaffold(),
-      // routes: {
-        // SplashScreen.routeName: (context) => const SplashScreen(),
-        // HomePage.routeName: (context) => const HomePage(),
-        // RewardCheckPage.routeName: (context) => const RewardCheckPage(),
-        // StatisticPage.routeName: (context) => const StatisticPage(),
-      // },
-      // initialRoute: HomePage.routeName,
-      // initialRoute: SplashScreen.routeName,
     );
   }
 }
